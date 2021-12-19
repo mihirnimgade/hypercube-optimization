b5fa36f41d91be9381db4e7937e4dcee6dbc73e0
mihirnimgade <mihir.nim@outlook.com>
2021-12-19 00:08:43 -0800

    Replace slice reference attributes in hypercube struct with Vec<f64>

--- src/hypercube.rs
+++ src/hypercube.rs
@@ -1,32 +1,50 @@
 use std::fmt;
+use rand::{thread_rng, Rng};
+use rand::distributions::{Uniform};
 
-// lifetime parameter suggests that the Hypercube struct only
-// lasts as long as the init_point reference
-pub struct Hypercube<'a> {
-    init_point: &'a[f64],
-    dimension: usize,
-    upper_bound: &'a[f64],
-    lower_bound: &'a[f64]
+pub struct Hypercube {
+    dimension: i64,
+    upper_bound: f64,
+    lower_bound: f64,
+    init_point: Vec<f64>,
+    population: Vec<Vec<f64>>
 }
 
-impl<'a> Hypercube<'a> {
-    pub fn new(init_point: &'a[f64], upper_bound: &'a[f64], lower_bound: &'a[f64]) -> Hypercube<'a> {
-        if upper_bound.len() == init_point.len() && lower_bound.len() == init_point.len() {
-            Hypercube {
-                init_point: init_point,
-                dimension: init_point.len(),
-                upper_bound: upper_bound,
-                lower_bound: lower_bound
-            }
-        } else {
-            panic!("Initial point and bounds arguments do not share the same size!");
+impl Hypercube {
+    pub fn new(dimension: i64, init_point: Vec<f64>, upper_bound: f64, lower_bound: f64) -> Self {
+        assert!(dimension as usize == init_point.len(), "Dimension incompatible with initial point length");
+        assert!(init_point.len() != 0, "Initial point is empty!");
+
+        // TODO: replace with function that takes dimension and bounds and returns number of hypercube points
+        let num_points = dimension;
+
+        let mut rng = thread_rng();
+        let uniform_range = Uniform::new_inclusive(lower_bound, upper_bound);
+
+        // random point Vector to store random generated points
+        let mut random_points : Vec<Vec<f64>> = Vec::new();
+
+        for _ in 0..num_points {
+            // generates random point using uniform distribution
+            let v: Vec<f64> = (&mut rng).sample_iter(uniform_range).take(dimension.try_into().unwrap()).collect();
+
+            // insert point into random_points vector
+            random_points.push(v);
+        }
+
+        Self {
+            dimension: dimension,
+            init_point: init_point,
+            upper_bound: upper_bound,
+            lower_bound: lower_bound,
+            population: random_points
         }
     }
 }
 
-impl fmt::Display for Hypercube<'_> {
+impl fmt::Display for Hypercube {
     fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Initial point: {:?}\n Dimension: {}\n
-        Lower bound: {:?}\n Upper bound: {:?}\n", self.init_point, self.dimension, self.lower_bound, self.upper_bound)
+        write!(f, "Initial point: {:#?}\nDimension: {}\nLower bound: {:?}\nUpper bound: {:?}\nPopulation points: {:#?}\n",
+            self.init_point, self.dimension, self.lower_bound, self.upper_bound, self.population)
     }
 }
