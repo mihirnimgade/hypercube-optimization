e197df0893925629b2d32502f90ceed791a73d08
mihirnimgade <mihir.nim@outlook.com>
2021-12-20 01:58:01 -0800

    Use evaluate() method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use rand::{thread_rng, Rng};
-use rand::distributions::{Uniform};
+use rand::{Rng, thread_rng};
+use rand::distributions::Uniform;
+
 use crate::objective_functions::objective_functions::rastrigin;
 
 mod hypercube;
@@ -10,8 +11,8 @@ fn generate_random_point(dimension: i64, lower_bound: f64, upper_bound: f64) -> 
     let mut rng = thread_rng();
     let uniform_range = Uniform::new_inclusive(lower_bound, upper_bound);
     let random_point: Vec<f64> = (&mut rng).sample_iter(uniform_range)
-                                           .take(dimension.try_into().unwrap())
-                                           .collect();
+        .take(dimension.try_into().unwrap())
+        .collect();
     random_point
 }
 
@@ -20,15 +21,19 @@ fn main() {
     // HypercubeOptimizer will create mutable Hypercube object and manipulate it within a loop
     // HypercubeOptimizer.run() should take an objective function and bounds
 
-    let dimension: i64 = 10;
+    let dimension: i64 = 1000;
     let lower_bound: f64 = 0.0;
     let upper_bound: f64 = 120.0;
 
     // generate random initial point
     let initial_point: Vec<f64> = generate_random_point(dimension, lower_bound, upper_bound);
 
-    let cube = hypercube::Hypercube::new(dimension, initial_point.clone(), upper_bound, lower_bound);
+    let mut cube = hypercube::Hypercube::new(dimension, initial_point.clone(), upper_bound, lower_bound);
 
-    let rastrigin_val: f64 = rastrigin(&initial_point);
-    println!("{}", rastrigin_val);
+    cube.evaluate(rastrigin);
+
+    match cube.values {
+        Some(_t) => println!("All values evaluated!"),
+        None => println!("Values not evaluated.")
+    }
 }
