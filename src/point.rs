d3b8f0d0e5e5d9621bb732176eaa957b7db2b4ec
mihirnimgade <mihir.nim@outlook.com>
2022-01-03 02:35:31 -0800

    Implement AddAssign trait and .iter() function for Point type

--- src/point.rs
+++ src/point.rs
@@ -4,8 +4,15 @@ added, subtracted, multiplied, and divided. Once a point is created, it has a de
 unchangeable dimension which corresponds to the capacity of the underlying Vec<f64> attribute.
  */
 
+use crate::bounds::Bounds;
 use crate::hypercube::cmp;
-use std::ops::{Add, Sub};
+
+use std::ops::{Add, AddAssign, Sub};
+
+use rand::distributions::Uniform;
+use rand::{thread_rng, Rng};
+
+use std::slice::Iter;
 
 #[derive(Debug, PartialOrd, PartialEq)]
 pub struct Point {
@@ -14,13 +21,21 @@ pub struct Point {
     coords: Vec<f64>,
 }
 
+/// Operation trait implementations
+
 impl Add for Point {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
         // points need to have the same dimension to be added
-        assert_eq!(self.dimension, other.dimension, "addition failed: operands do not have same dimension");
-        assert_ne!(self.dimension, 0, "addition failed: point dimension cannot be zero");
+        assert_eq!(
+            self.dimension, other.dimension,
+            "addition failed: operands do not have same dimension"
+        );
+        assert_ne!(
+            self.dimension, 0,
+            "addition failed: point dimension cannot be zero"
+        );
 
         let mut add_result = Vec::new();
 
@@ -32,13 +47,32 @@ impl Add for Point {
     }
 }
 
+impl AddAssign for Point {
+    fn add_assign(&mut self, rhs: Self) {
+        for (index, element) in self.coords.iter_mut().enumerate() {
+            *element += rhs.coords.get(index).unwrap();
+        }
+
+        // recompute mathematical length
+        let length = self.coords.iter().fold(0.0, |acc, x| acc + (x * x)).sqrt();
+
+        self.length = length;
+    }
+}
+
 impl Sub for Point {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
         // points need to have the same dimension to be subtracted
-        assert_eq!(self.dimension, other.dimension, "subtraction failed: operands do not have same dimension");
-        assert_ne!(self.dimension, 0, "addition failed: point dimension cannot be zero");
+        assert_eq!(
+            self.dimension, other.dimension,
+            "subtraction failed: operands do not have same dimension"
+        );
+        assert_ne!(
+            self.dimension, 0,
+            "addition failed: point dimension cannot be zero"
+        );
 
         let mut sub_result = Vec::new();
 
@@ -50,20 +84,10 @@ impl Sub for Point {
     }
 }
 
-impl IntoIterator for Point {
-    type Item = f64;
-    type IntoIter = std::vec::IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.coords.into_iter()
-    }
-}
+/// Struct method implementations
 
 impl Point {
-
-    /*
-    Creates a Point struct from a vector. Consumes vector in the process.
-     */
+    /// Creates a Point struct from a vector. Consumes vector in the process.
     pub fn from_vec(vector: Vec<f64>) -> Self {
         assert_ne!(vector.len(), 0, "vector dimension cannot be zero");
 
@@ -79,9 +103,7 @@ impl Point {
         }
     }
 
-    /*
-    Creates a Point and initializes its coordinates with `element` and a dimension of `n`.
-     */
+    /// Creates a Point and initializes its coordinates with `element` and a dimension of `n`.
     pub fn fill(element: f64, n: u32) -> Self {
         let coords = vec![element; n as usize];
 
@@ -91,46 +113,46 @@ impl Point {
         Self {
             dimension: n,
             length,
-            coords
+            coords,
         }
     }
 
-    fn push(&mut self, value: f64) -> Result<(), String> {
-        // if the coordinate is full
-        if self.coords.len() >= self.coords.capacity() {
-            Err(format!("cannot push value into Point struct, dimension limit of {} reached", self.dimension))
-        } else {
-            self.coords.push(value);
+    /// Creates a Point with random coordinates within given bounds.
+    pub fn random(dimension: u32, bounds: Bounds) -> Self {
+        let mut rng = thread_rng();
+        let uniform_range = Uniform::new_inclusive(bounds.lower, bounds.upper);
 
-            // recompute length
-            self.length = self.coords.iter().fold(0.0, |acc, x| acc + (x * x)).sqrt();
+        let random_vec: Vec<f64> = (&mut rng)
+            .sample_iter(uniform_range)
+            .take(dimension.try_into().unwrap())
+            .collect();
 
-            Ok(())
-        }
+        Self::from_vec(random_vec)
     }
 
-    fn pop(&mut self) -> Option<f64> {
-        let result = self.coords.pop();
-
-        // recompute length
-        self.length = self.coords.iter().fold(0.0, |acc, x| acc + (x * x)).sqrt();
-
-        result
+    pub fn dim(&self) -> u32 {
+        self.dimension
     }
 
     pub fn get(&self, index: usize) -> Option<&f64> {
         self.coords.get(index)
     }
 
-    pub fn max(&self) -> Option<f64> {
+    pub fn max_val(&self) -> Option<f64> {
         self.coords.iter().copied().max_by(cmp)
     }
 
-    pub fn min(&self) -> Option<f64> {
+    pub fn min_val(&self) -> Option<f64> {
         self.coords.iter().copied().min_by(cmp)
+    }
+
+    pub fn iter(&self) -> Iter<'_, f64> {
+        self.coords.iter()
     }
 }
 
+/// Point creation macro
+#[macro_export]
 macro_rules! point {
     ( $( $x:expr ),*) => {
         {
@@ -156,7 +178,10 @@ mod point_tests {
 
     #[test]
     fn make_new_point_from_vec() {
-        assert_eq!(Point::from_vec(vec![5.2, 4.5, 33.2]), Point::from_vec(vec![5.2, 4.5, 33.2]));
+        assert_eq!(
+            Point::from_vec(vec![5.2, 4.5, 33.2]),
+            Point::from_vec(vec![5.2, 4.5, 33.2])
+        );
     }
 
     #[test]
@@ -199,7 +224,7 @@ mod point_tests {
 
     #[test]
     fn compute_length_3() {
-        let mut a = Point::from_vec(vec![4.9, 32.2, 3.1, 889.1]);
+        let a = Point::from_vec(vec![4.9, 32.2, 3.1, 889.1]);
 
         let expected_length = (791569.27_f64).sqrt();
 
@@ -224,5 +249,25 @@ mod point_tests {
         let c = point![0.0; 5];
 
         assert_eq!(a - b, c);
+    }
+
+    #[test]
+    fn add_assign_1() {
+        let mut a = Point::fill(3.0, 4);
+        let b = point![2.3, 4.3, 1.2, 6.7];
+
+        a += b;
+
+        assert_eq!(a, point![5.3, 7.3, 4.2, 9.7]);
+    }
+
+    #[test]
+    fn add_assign_2() {
+        let mut a = Point::fill(5.6, 10);
+        let b = Point::fill(4.4, 10);
+
+        a += b;
+
+        assert_eq!(a, point![10.0; 10]);
     }
 }
