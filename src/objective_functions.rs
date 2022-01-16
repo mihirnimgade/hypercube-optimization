eb55fcee60dd770a17d05f2623df2dc529b2902d
mihirnimgade <mihir.nim@outlook.com>
2022-01-15 21:13:26 -0800

    Reformat file

--- src/objective_functions.rs
+++ src/objective_functions.rs
@@ -2,15 +2,15 @@
 
 pub mod objective_functions {
 
-    use std::f64::consts::PI;
     use crate::point::Point;
+    use std::f64::consts::PI;
 
     pub fn rastrigin(input_point: &Point) -> f64 {
         let dimension = input_point.dimension;
         let mut sum: f64 = 0.0;
 
         for val in input_point.iter() {
-            sum += val.powf(2.0) - (10.0 * (2.0*PI*val).cos());
+            sum += val.powf(2.0) - (10.0 * (2.0 * PI * val).cos());
         }
 
         10.0 * dimension as f64 + sum
