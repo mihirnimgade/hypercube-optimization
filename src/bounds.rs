f8d8b683cfca05a120b65eb5f94546fd0434e733
mihirnimgade <mihir.nim@outlook.com>
2022-01-15 21:10:11 -0800

    Write unit tests for hypercube bounds

--- src/bounds.rs
+++ src/bounds.rs
@@ -79,7 +79,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn make_new_bounds_1() {
+    fn new_bounds_1() {
         let a = HypercubeBounds::new(5, 0.0, 10.0);
 
         let b = HypercubeBounds {
@@ -92,13 +92,134 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn make_new_bounds_2() {
+    fn new_bounds_2() {
         let _a = HypercubeBounds::new(5, 10.0, 0.0);
     }
 
     #[test]
     #[should_panic]
-    fn make_new_bounds_3() {
+    fn new_bounds_3() {
         let _a = HypercubeBounds::new(5, 10.0, 10.0);
+    }
+
+    #[test]
+    fn within_subset() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let b = HypercubeBounds::new(3, -10.0, 200.0);
+
+        assert_eq!(a.within(&b), true);
+    }
+
+    #[test]
+    fn within_equal() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+
+        assert_eq!(a.within(&a), true);
+    }
+
+    #[test]
+    fn not_within_right_overlap() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let b = HypercubeBounds::new(3, 100.0, 200.0);
+
+        assert_eq!(a.within(&b), false);
+    }
+
+    #[test]
+    fn not_within_left_overlap() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let b = HypercubeBounds::new(3, -10.0, 90.0);
+
+        assert_eq!(a.within(&b), false);
+    }
+
+    #[test]
+    fn not_within_superset() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let b = HypercubeBounds::new(3, 30.0, 90.0);
+
+        assert_eq!(a.within(&b), false);
+    }
+
+    #[test]
+    fn not_within() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let b = HypercubeBounds::new(3, -10.0, -5.0);
+
+        assert_eq!(a.within(&b), false);
+    }
+
+    #[test]
+    fn displace_by_1() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let displacement_vec = point![1.0, 22.3, 11.7];
+
+        let calc_result = a.displace_by(&displacement_vec);
+
+        let expected_result = HypercubeBounds::from_points(
+        point![1.0, 22.3, 11.7],
+        point![121.0, 142.3, 131.7]
+        );
+
+        assert_eq!(expected_result, calc_result);
+    }
+
+    #[test]
+    fn displace_by_2() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let displacement_vec = point![0.0; 3];
+
+        let calc_result = a.displace_by(&displacement_vec);
+
+        let expected_result = HypercubeBounds::from_points(
+            point![0.0; 3],
+            point![120.0; 3]
+        );
+
+        assert_eq!(expected_result, calc_result);
+    }
+
+    #[test]
+    fn displace_by_in_place_1() {
+        let mut a = HypercubeBounds::new(3, 0.0, 120.0);
+        let displacement_vec = point![1.0, 22.3, 11.7];
+
+        a.displace_by_in_place(&displacement_vec);
+
+        let expected_result = HypercubeBounds::from_points(
+            point![1.0, 22.3, 11.7],
+            point![121.0, 142.3, 131.7]
+        );
+
+        assert_eq!(expected_result, a);
+    }
+
+    #[test]
+    fn displace_by_in_place_2() {
+        let mut a = HypercubeBounds::new(3, 0.0, 120.0);
+        let displacement_vec = point![0.0; 3];
+
+        a.displace_by_in_place(&displacement_vec);
+
+        let expected_result = HypercubeBounds::from_points(
+            point![0.0; 3],
+            point![120.0; 3]
+        );
+
+        assert_eq!(expected_result, a);
+    }
+
+    #[test]
+    fn scale_1() {
+        let mut a = HypercubeBounds::new(3, 50.0, 120.0);
+
+        a.scale(1.0/10.0);
+
+        let expected_result = HypercubeBounds::from_points(
+            point![5.0; 3],
+            point![12.0; 3]
+        );
+
+        assert_eq!(expected_result, a);
     }
 }
