75662d86088807e0fd06ffdad223083d2af91a7e
mihirnimgade <mihir.nim@outlook.com>
2022-01-17 01:07:01 -0800

    Implement scale_in_place() and scale_towards_center() along with unit tests

--- src/bounds.rs
+++ src/bounds.rs
@@ -9,7 +9,7 @@ use crate::point::Point;
 #[derive(Clone, Debug, PartialEq)]
 pub struct HypercubeBounds {
     lower: Point,
-    upper: Point
+    upper: Point,
 }
 
 impl HypercubeBounds {
@@ -59,7 +59,7 @@ impl HypercubeBounds {
     pub fn displace_by(&self, vector: &Point) -> Self {
         Self {
             lower: &self.lower + vector,
-            upper: &self.upper + vector
+            upper: &self.upper + vector,
         }
     }
 
@@ -69,9 +69,27 @@ impl HypercubeBounds {
         self.upper = &self.upper + vector;
     }
 
-    pub fn scale(&mut self, scale_factor: f64) {
-        self.lower.scale(scale_factor);
-        self.upper.scale(scale_factor);
+    /// Scale lower and upper bounds by a scale factor
+    pub fn scale_in_place(&mut self, scale_factor: f64) {
+        self.lower.scale_in_place(scale_factor);
+        self.upper.scale_in_place(scale_factor);
+    }
+
+    /// Scale bounds towards center of hypercube
+    pub fn shrink_towards_center(&self, center: &Point, scale_factor: f64) -> Self {
+        assert!(scale_factor >= 0.0, "negative scale factor is invalid");
+        assert!(scale_factor <= 1.0, "scale factor above 1 is invalid");
+
+        let lower_to_center: Point = center - &self.lower;
+        let upper_to_center: Point = center - &self.upper;
+
+        let scaled_lower_to_center = lower_to_center.scale(1.0 - scale_factor);
+        let scaled_upper_to_center = upper_to_center.scale(1.0 - scale_factor);
+
+        Self {
+            lower: &self.lower + &scaled_lower_to_center,
+            upper: &self.upper + &scaled_upper_to_center,
+        }
     }
 }
 
@@ -100,6 +118,12 @@ mod tests {
     #[should_panic]
     fn new_bounds_3() {
         let _a = HypercubeBounds::new(5, 10.0, 10.0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn new_bounds_4() {
+        let _a = HypercubeBounds::new(0, 0.0, 10.0);
     }
 
     #[test]
@@ -156,10 +180,8 @@ mod tests {
 
         let calc_result = a.displace_by(&displacement_vec);
 
-        let expected_result = HypercubeBounds::from_points(
-        point![1.0, 22.3, 11.7],
-        point![121.0, 142.3, 131.7]
-        );
+        let expected_result =
+            HypercubeBounds::from_points(point![1.0, 22.3, 11.7], point![121.0, 142.3, 131.7]);
 
         assert_eq!(expected_result, calc_result);
     }
@@ -171,10 +193,7 @@ mod tests {
 
         let calc_result = a.displace_by(&displacement_vec);
 
-        let expected_result = HypercubeBounds::from_points(
-            point![0.0; 3],
-            point![120.0; 3]
-        );
+        let expected_result = HypercubeBounds::from_points(point![0.0; 3], point![120.0; 3]);
 
         assert_eq!(expected_result, calc_result);
     }
@@ -186,10 +205,8 @@ mod tests {
 
         a.displace_by_in_place(&displacement_vec);
 
-        let expected_result = HypercubeBounds::from_points(
-            point![1.0, 22.3, 11.7],
-            point![121.0, 142.3, 131.7]
-        );
+        let expected_result =
+            HypercubeBounds::from_points(point![1.0, 22.3, 11.7], point![121.0, 142.3, 131.7]);
 
         assert_eq!(expected_result, a);
     }
@@ -201,25 +218,69 @@ mod tests {
 
         a.displace_by_in_place(&displacement_vec);
 
-        let expected_result = HypercubeBounds::from_points(
-            point![0.0; 3],
-            point![120.0; 3]
-        );
+        let expected_result = HypercubeBounds::from_points(point![0.0; 3], point![120.0; 3]);
 
         assert_eq!(expected_result, a);
     }
 
     #[test]
-    fn scale_1() {
+    fn scale_in_place_1() {
         let mut a = HypercubeBounds::new(3, 50.0, 120.0);
 
-        a.scale(1.0/10.0);
+        a.scale_in_place(0.0);
 
-        let expected_result = HypercubeBounds::from_points(
-            point![5.0; 3],
-            point![12.0; 3]
-        );
+        let expected_result = HypercubeBounds::from_points(point![0.0; 3], point![0.0; 3]);
 
         assert_eq!(expected_result, a);
+    }
+
+    #[test]
+    fn shrink_towards_center_1() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let center = point![60.0; 3];
+
+        let b = a.shrink_towards_center(&center, 0.5);
+        let expected_result = HypercubeBounds::new(3, 30.0, 90.0);
+
+        assert_eq!(expected_result, b);
+    }
+
+    #[test]
+    fn shrink_towards_center_2() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let center = point![60.0; 3];
+
+        let b = a.shrink_towards_center(&center, 0.0);
+        let expected_result = HypercubeBounds::from_points(center.clone(), center.clone());
+
+        assert_eq!(expected_result, b);
+    }
+
+    #[test]
+    fn shrink_towards_center_3() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let center = point![60.0; 3];
+
+        let b = a.shrink_towards_center(&center, 1.0);
+
+        assert_eq!(a, b);
+    }
+
+    #[test]
+    #[should_panic]
+    fn shrink_towards_center_4() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let center = point![60.0; 3];
+
+        let _b = a.shrink_towards_center(&center, 1.1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn shrink_towards_center_5() {
+        let a = HypercubeBounds::new(3, 0.0, 120.0);
+        let center = point![60.0; 3];
+
+        let _b = a.shrink_towards_center(&center, -0.1);
     }
 }
