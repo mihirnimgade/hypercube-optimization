abc9182723a815f54f5470d5a32543bc030d4049
mihirnimgade <mihir.nim@outlook.com>
2022-01-27 00:07:25 -0800

    Add unit tests

--- src/hypercube.rs
+++ src/hypercube.rs
@@ -245,6 +245,42 @@ mod tests {
     }
 
     #[test]
+    #[should_panic]
+    fn displace_by_2() {
+        let mut test_hypercube = Hypercube::new(5, 30.4, 105.0);
+        test_hypercube.shrink(0.5);
+        let small_vector = point![0.01; 7];
+
+        test_hypercube.displace_by(&small_vector);
+    }
+
+    #[test]
+    fn displace_by_3() {
+        let mut test_hypercube = Hypercube::new(5, 30.4, 105.0);
+        let small_vector = point![0.01; 5];
+
+        test_hypercube.shrink(0.90);
+        assert!(test_hypercube.displace_by(&small_vector).is_ok());
+    }
+
+    #[test]
+    fn displace_to_1() {
+        let mut test_hypercube = Hypercube::new(5, 0.0, 105.0);
+        let small_vector = point![52.6; 5];
+
+        assert!(test_hypercube.displace_to(&small_vector).is_err());
+    }
+
+    #[test]
+    #[should_panic]
+    fn displace_to_2() {
+        let mut test_hypercube = Hypercube::new(5, 30.4, 105.0);
+        let small_vector = point![0.01; 7];
+
+        test_hypercube.displace_to(&small_vector);
+    }
+
+    #[test]
     fn shrink_and_displace_1() {
         let mut test_hypercube = Hypercube::new(5, 0.0, 120.0);
         let small_vector = point![1.0; 5];
