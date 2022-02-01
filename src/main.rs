12b30efd4de87e5ae752d40439505d150499e4e9
mihirnimgade <mihir.nim@outlook.com>
2022-01-31 18:02:57 -0800

    Update user code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use crate::objective_functions::objective_functions::rastrigin;
+use crate::optimizer::HypercubeOptimizer;
 use crate::point::Point;
 
 mod bounds;
+mod evaluation;
 mod hypercube;
 mod objective_functions;
 mod optimizer;
@@ -9,35 +11,22 @@ mod point;
 mod result;
 
 fn main() {
-    // create HypercubeOptimizer object here with certain parameters
-    // HypercubeOptimizer will create mutable Hypercube object and manipulate it within a loop
-    // HypercubeOptimizer.run() should take an objective function and bounds
+    let dimension = 8;
+    let initial_point = point![60.0; dimension];
+    let lower_bound = 0.0;
+    let upper_bound = 120.0;
 
-    let dimension: u32 = 8;
+    // create HypercubeOptimizer with certain parameters
+    let optimizer = HypercubeOptimizer::new(
+        initial_point,
+        lower_bound,
+        upper_bound,
+        rastrigin,
+        0.01,
+        0.01,
+        4000,
+        120,
+    );
 
-    let mut cube = hypercube::Hypercube::new(dimension, 0.0, 120.0);
-
-    let s = "-".repeat(20);
-
-    println!("{} pre-evaluation and pre-shrink {}\n", s, s);
-    println!("{}", cube);
-
-    cube.evaluate(rastrigin);
-
-    println!("Best value: {}", cube.peek_best_value().unwrap());
-
-    // shrink and displace
-    cube.shrink(0.01);
-    let result = cube.displace_by(&point![1.0; dimension]);
-
-    match result {
-        Ok(()) => println!("displacement successful!\n"),
-        Err(str) => println!("{}\n", str),
-    }
-
-    cube.evaluate(rastrigin);
-
-    println!("{} post-evaluation and post-shrink {}\n", s, s);
-    println!("{}", cube);
-    println!("Best value: {}", cube.peek_best_value().unwrap());
+    // HypercubeOptimizer.optimize() should not take any arguments and should return HypercubeOptimizerResult struct
 }
