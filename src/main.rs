1a2e6dcb0ba45738114c6ee381351e9deb4af3a4
mihirnimgade <mihir.nim@outlook.com>
2022-03-18 00:12:58 -0700

    Use neg_rastrigin function in optimizer example

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-use hypercube_optimizer::objective_functions::rastrigin;
+use hypercube_optimizer::objective_functions::{neg_rastrigin, rastrigin};
 use hypercube_optimizer::optimizer::HypercubeOptimizer;
 use hypercube_optimizer::point;
 use hypercube_optimizer::point::Point;
 
 fn main() {
     let dimension = 3;
-    let initial_point = point![60.0; dimension];
-    let lower_bound = 0.0;
-    let upper_bound = 120.0;
+    let initial_point = point![4.0; dimension];
+    let lower_bound = -5.0;
+    let upper_bound = 5.0;
 
     // create HypercubeOptimizer with certain parameters
     let mut optimizer = HypercubeOptimizer::new(
         initial_point,
         lower_bound,
         upper_bound,
-        rastrigin,
+        neg_rastrigin,
         0.01,
         0.01,
-        4000,
+        200,
         120,
     );
 
