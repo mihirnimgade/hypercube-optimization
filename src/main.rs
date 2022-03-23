89ec8ea5544c12ddd4b04633c88625c9a23ff3bd
mihirnimgade <mihir.nim@outlook.com>
2022-03-23 01:15:38 -0700

    Update main user code

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use hypercube_optimizer::point;
 use hypercube_optimizer::point::Point;
 
 fn main() {
-    let dimension = 3;
-    let initial_point = point![4.0; dimension];
-    let lower_bound = -5.0;
-    let upper_bound = 5.0;
+    let dimension = 8;
+    let initial_point = point![60.0; dimension];
+    let lower_bound = 0.0;
+    let upper_bound = 120.0;
 
     // create HypercubeOptimizer with certain parameters
     let mut optimizer = HypercubeOptimizer::new(
@@ -17,13 +17,18 @@ fn main() {
         neg_rastrigin,
         0.01,
         0.01,
-        200,
+        4000,
         120,
     );
 
     let result = optimizer.maximize();
 
-    println!("Final result: {:?}", result);
-
-    // HypercubeOptimizer.optimize() should not take any arguments and should return HypercubeOptimizerResult struct
+    match result {
+        None => {
+            println!("Unable to determine final result")
+        }
+        Some(t) => {
+            println!("Final result: {:?}", t);
+        }
+    }
 }
