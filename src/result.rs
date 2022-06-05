531c2d617232b7b84ac4a102ef2587b6c6a55624
Mihir Nimgade <mihir.nim@outlook.com>
2022-06-04 21:30:09 -0700

    #11 - Implement HypercubeOptimizerResult type

--- src/result.rs
+++ src/result.rs
@@ -1,15 +1,56 @@
-use crate::point::Point;
+use crate::{point::Point, evaluation::PointEval};
 
+/// Exit codes:
+/// 0 => successful execution
+/// 1 => general optimization error
+/// 2 => non-convergence within defined bounds
+/// 3 => optimization timeout
+/// 4 => optimization bounds are too large
+
+
+#[derive(Debug)]
 pub struct HypercubeOptimizerResult {
-    result: String,
+    exit_code: u32,
+    message: &'static str,
     loops: u32,
-    function_evals: u32,
-    best_x: Point,
-    best_f: f64,
+    fn_evals: u32,
+    best_x: Option<Point>,
+    best_f: Option<f64>,
 }
 
 impl HypercubeOptimizerResult {
-    pub fn new() -> Self {
-        todo!()
+    pub fn new(
+        exit_code: u32,
+        loops: u32,
+        fn_evals: u32,
+        best_value: Option<&PointEval>,
+    ) -> Self {
+        // map exit code to message
+        let message = Self::map_to_message(exit_code);
+
+        // separate best value into point and eval
+
+        let best_f = best_value.map(|v| v.get_eval());
+        let best_x = best_value.map(|v| v.get_point());
+
+        Self {
+            exit_code,
+            message,
+            loops,
+            fn_evals,
+            best_x,
+            best_f,
+        }
+    }
+
+    pub fn map_to_message(exit_code: u32) -> &'static str {
+        match exit_code {
+            0 => "optimization successful",
+            1 => "general optimization error",
+            2 => "non-convergence within defined bounds",
+            3 => "optimization timeout",
+            4 => "optimization bounds are too large",
+            _ => "",
+        }
     }
 }
